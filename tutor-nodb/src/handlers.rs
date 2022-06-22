c02ddbebc1c04f42d32aae0b2f2069c37063b0cb
siva-centime <siva@centime.com>
2022-06-22 09:05:32 +0530

    add route and handler for retreiving course details

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -57,10 +57,31 @@ pub async fn get_courses_for_tutor(
     }
 }
 
+pub async fn get_course_detail(
+    app_state: web::Data<AppState>,
+    params: web::Path<(usize, usize)>,
+) -> HttpResponse {
+    let (tutor_id, course_id) = params.0;
+    let selected_course = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .find(|x| x.tutor_id == tutor_id && x.course_id == Some(course_id))
+        .ok_or("Course not found");
+    
+    if let Ok(course) = selected_course {
+        HttpResponse::Ok().json(course)
+    } else {
+        HttpResponse::Ok().json("Course not found".to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use actix_web::http::StatusCode;
+    use actix_web::{http::StatusCode, App};
     use std::sync::Mutex;
 
     #[actix_rt::test]
@@ -89,6 +110,18 @@ mod tests {
         });
         let tutor_id: web::Path<usize> = web::Path::from(1);
         let resp = get_courses_for_tutor(app_state, tutor_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_one_course_success() {
+        let app_state: web::Data<AppState> = web::Data::new(AppState { 
+            health_check_response: "".to_string(), 
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+        let params: web::Path<(usize, usize)> = web::Path::from((1,1));
+        let resp = get_course_detail(app_state, params).await;
         assert_eq!(resp.status(), StatusCode::OK);
     }
 }
