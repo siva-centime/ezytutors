8e8e10959c657ea2fc60050fdb0f5f578d08a402
siva-centime <siva@centime.com>
2022-06-22 08:51:54 +0530

    add route and handler for getting course list by tutor

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -35,6 +35,28 @@ pub async fn new_course(
     HttpResponse::Ok().json("Added course")
 }
 
+pub async fn get_courses_for_tutor(
+    app_state: web::Data<AppState>,
+    params: web::Path<usize>,
+) -> HttpResponse {
+    let tutor_id: usize = params.0;
+
+    let filtered_courses = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.tutor_id == tutor_id)
+        .collect::<Vec<Course>>();
+    
+    if filtered_courses.len() > 0 {
+        HttpResponse::Ok().json(filtered_courses)
+    } else {
+        HttpResponse::Ok().json("No courses found for tutor".to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,6 +77,18 @@ mod tests {
             courses: Mutex::new(vec![]) 
         });
         let resp = new_course(course, app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_all_courses_success() {
+        let app_state: web::Data<AppState> = web::Data::new(AppState { 
+            health_check_response: "".to_string(), 
+            visit_count: Mutex::new(0), 
+            courses: Mutex::new(vec![]) 
+        });
+        let tutor_id: web::Path<usize> = web::Path::from(1);
+        let resp = get_courses_for_tutor(app_state, tutor_id).await;
         assert_eq!(resp.status(), StatusCode::OK);
     }
 }
--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -8,5 +8,7 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg
         .service(web::scope("/courses")
-        .route("/", web::post().to(new_course)));
+        .route("/", web::post().to(new_course))
+        .route("/{user_id}", web::get().to(get_courses_for_tutor)),
+    );
 }
